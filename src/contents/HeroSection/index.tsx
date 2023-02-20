5101596679be81e3273c8e81c0a67b16319f7e71
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-20 20:56:08 -0300

    🚧 HeroSection created

---
+++ src/contents/HeroSection/index.tsx
@@ -0,0 +1,18 @@
+export function HeroSection() {
+  return (
+    <section id="hero" className="min-h-[532px] bg-[#F8F8FF]">
+      <span>Find your block</span>
+      <h1>
+        Encontre os <span>melhores blocos</span> de carnaval de 2023
+      </h1>
+
+      <div>
+        <input type="search" name="" id="" />
+        <select>
+          <option value="1">1</option>
+        </select>
+        <button>Buscar agora</button>
+      </div>
+    </section>
+  )
+}
