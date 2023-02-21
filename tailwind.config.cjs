e7badac66a3c325a744660133d0cb506bb204337
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-21 09:26:55 -0300

    💄 add colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				white: '#FFFFFF',
+				black: {
+					900: '#121214',
+					800: '#000000bf',
+				},
+				red: {
+					500: '#E45858',
+				},
+				violet: {
+					500: '#6246EA',
+				},
+			}
+		},
 	},
 	plugins: [],
 }
