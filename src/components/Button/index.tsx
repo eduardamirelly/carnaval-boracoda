f06c5640263bf3b5886ea2180487eedac8e7a93c
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-20 21:36:15 -0300

    🎨 create Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+export function Button({ children }: ButtonProps) {
+  return (
+    <button>
+      {children}
+    </button>
+  )
+}
