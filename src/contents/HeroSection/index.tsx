0e4135b0cc3e504a79788950c8e97c135cdddf64
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-22 22:12:21 -0300

    💄 added images to page

--- src/contents/HeroSection/index.tsx
+++ src/contents/HeroSection/index.tsx
@@ -1,13 +1,15 @@
 export function HeroSection() {
   return (
-    <section id="hero" className="min-h-[532px] bg-[#F8F8FF] flex flex-col items-center justify-center">
+    <section id="hero" className="min-h-[532px] bg-[#F8F8FF] flex flex-col items-center justify-center relative">
       <span className="font-medium uppercase text-red-500 tracking-widest">Find your block</span>
       <h1 className="font-bold text-5xl text-black-900">
         Encontre os <span className="text-violet-500">melhores blocos</span> de carnaval de 2023
       </h1>
 
-      <img src="/assets/ilustra-01.png" />
-      <img src="/assets/ilustra-02.png" />
+      <div className="absolute inset-0 w-full flex justify-between item">
+        <img className="object-cover h-[330px]" src="/assets/ilustra-01.svg" />
+        <img className="self-end object-cover h-[330px]" src="/assets/ilustra-02.svg" />
+      </div>
 
       <div>
         <input type="search" name="" id="" />
