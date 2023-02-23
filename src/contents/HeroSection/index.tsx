4ffa3823fb51edff3384b5389579986b9d8ef955
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-22 22:00:38 -0300

    🍱 move assets to public folder

--- src/contents/HeroSection/index.tsx
+++ src/contents/HeroSection/index.tsx
@@ -1,10 +1,13 @@
 export function HeroSection() {
   return (
-    <section id="hero" className="min-h-[532px] bg-[#F8F8FF]">
+    <section id="hero" className="min-h-[532px] bg-[#F8F8FF] flex flex-col items-center justify-center">
       <span className="font-medium uppercase text-red-500 tracking-widest">Find your block</span>
       <h1 className="font-bold text-5xl text-black-900">
         Encontre os <span className="text-violet-500">melhores blocos</span> de carnaval de 2023
       </h1>
+
+      <img src="/assets/ilustra-01.png" />
+      <img src="/assets/ilustra-02.png" />
 
       <div>
         <input type="search" name="" id="" />
