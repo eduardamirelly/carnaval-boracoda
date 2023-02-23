a50156fd8c7093ee5f4f9e85ea8c34aa88c8e6f2
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-22 22:23:00 -0300

    💄 Button updated

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,26 @@
 import { ReactNode } from "react"
+import { clsx } from "clsx";
 
 interface ButtonProps {
   children: ReactNode;
+  color?: 'violet' | 'white';
+  className?: string;
 }
 
-export function Button({ children }: ButtonProps) {
+export function Button({ children, color = 'violet', className }: ButtonProps) {
   return (
-    <button>
+    <button 
+      className={
+        clsx(
+          'px-8 py-3 uppercase font-bold text-sm rounded cursor-pointer transition-colors', 
+          {
+            'bg-violet-500 text-white hover:bg-violet-600': color == 'violet',
+            'bg-white text-violet-500 hover:bg-black-300': color === 'white',
+          }, 
+          className
+        )
+      }
+    >
       {children}
     </button>
   )
--- src/contents/HeroSection/index.tsx
+++ src/contents/HeroSection/index.tsx
@@ -1,12 +1,14 @@
+import { Button } from "../../components/Button";
+
 export function HeroSection() {
   return (
-    <section id="hero" className="min-h-[532px] bg-[#F8F8FF] flex flex-col items-center justify-center relative">
+    <section id="hero" className="min-h-[532px] bg-[#F8F8FF] flex flex-col items-center justify-center relative z-10">
       <span className="font-medium uppercase text-red-500 tracking-widest">Find your block</span>
       <h1 className="font-bold text-5xl text-black-900">
         Encontre os <span className="text-violet-500">melhores blocos</span> de carnaval de 2023
       </h1>
 
-      <div className="absolute inset-0 w-full flex justify-between item">
+      <div className="absolute inset-0 w-full flex justify-between item -z-10">
         <img className="object-cover h-[330px]" src="/assets/ilustra-01.svg" />
         <img className="self-end object-cover h-[330px]" src="/assets/ilustra-02.svg" />
       </div>
@@ -16,7 +18,9 @@ export function HeroSection() {
         <select>
           <option value="1">1</option>
         </select>
-        <button>Buscar agora</button>
+        <Button>
+          Buscar agora
+        </Button>
       </div>
     </section>
   )
