56d42bf1e211b00c5f9e438245f5beffe27678cb
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-24 21:18:34 -0300

    🚧 header Blocks content

--- src/contents/Blocks/index.tsx
+++ src/contents/Blocks/index.tsx
@@ -1,7 +1,20 @@
+import { Button } from "../../components/Button";
+
 export function Blocks() {
   return (
-    <h1>
-      Blocks
-    </h1>
+    <section className="px-32 py-24">
+      <header className="flex justify-between items-center">
+        <h2 className="font-bold text-xl">Blocos recomendados</h2>
+
+        <div className="flex gap-1 items-center border border-gray-200 rounded p-2">
+          <Button size="sm">
+            Lista
+          </Button>
+          <Button size="sm" color="white">
+            Mapa
+          </Button>
+        </div>
+      </header>
+    </section>
   )
 }
