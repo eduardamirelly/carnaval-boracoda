019299c195967662531140dac93bc90285b563de
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-24 21:18:00 -0300

    🎨 update Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,18 +4,21 @@ import { clsx } from "clsx";
 interface ButtonProps {
   children: ReactNode;
   color?: 'violet' | 'white';
+  size?: 'lg' | 'sm';
   className?: string;
 }
 
-export function Button({ children, color = 'violet', className }: ButtonProps) {
+export function Button({ children, color = 'violet', size = 'lg', className }: ButtonProps) {
   return (
     <button 
       className={
         clsx(
-          'px-8 py-3 uppercase font-bold text-sm rounded cursor-pointer transition-colors', 
+          'uppercase font-bold text-sm rounded cursor-pointer transition-colors', 
           {
             'bg-violet-500 text-white hover:bg-violet-600': color == 'violet',
             'bg-white text-violet-500 hover:bg-gray-400': color === 'white',
+            'px-8 py-3': size == 'lg',
+            'px-6 py-1': size == 'sm',
           }, 
           className
         )
