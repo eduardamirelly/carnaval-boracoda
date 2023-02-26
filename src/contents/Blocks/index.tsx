d4965d7b6c9113823eb477169c252454d25ad464
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-25 21:18:36 -0300

    🚧 use Block component on Blocks content

--- src/contents/Blocks/index.tsx
+++ src/contents/Blocks/index.tsx
@@ -1,9 +1,10 @@
+import { Block } from "../../components/Block";
 import { Button } from "../../components/Button";
 
 export function Blocks() {
   return (
     <section className="px-32 py-24">
-      <header className="flex justify-between items-center">
+      <header className="flex justify-between items-center mb-10">
         <h2 className="font-bold text-xl">Blocos recomendados</h2>
 
         <div className="flex gap-1 items-center border border-gray-200 rounded p-2">
@@ -15,6 +16,13 @@ export function Blocks() {
           </Button>
         </div>
       </header>
+
+      <div className="flex justify-between gap-8 flex-wrap">
+        <Block />
+        <Block />
+        <Block />
+        <Block />
+      </div>
     </section>
   )
 }
