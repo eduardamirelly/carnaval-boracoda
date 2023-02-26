62b6159d712b588e1570f2232fa67388f0b444f4
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-25 21:23:10 -0300

    🎨 update Block component

--- src/components/Block/index.tsx
+++ src/components/Block/index.tsx
@@ -1,17 +1,25 @@
+import { MapPin } from 'phosphor-react';
 import { Text } from '../Text';
 
 export function Block() {
   return (
-    <div className="border border-gray-200 rounded-lg min-w-[384px] overflow-hidden">
+    <div className="border border-gray-200 rounded-lg w-[384px] overflow-hidden">
       <div className="max-h-[153px]">
         <img src="/assets/example-block.jpg" alt="example-block" />
       </div>
 
-      <div className="p-4">
+      <div className="p-4 flex flex-col gap-4">
         <h4 className="font-bold text-lg">Title Block</h4>
         <Text>
-          Description
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
         </Text>
+
+        <div className="flex items-center gap-2">
+          <MapPin className="text-red-500" size={24} />
+          <Text>
+            São Paulo - SP
+          </Text>
+        </div>
       </div>
     </div>
   )
