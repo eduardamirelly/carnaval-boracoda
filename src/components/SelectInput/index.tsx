c289deed3a6123bbe95e02175b7912f5591972ee
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-28 21:48:04 -0300

    🎨 update selectInput

--- src/components/SelectInput/index.tsx
+++ src/components/SelectInput/index.tsx
@@ -1,10 +1,10 @@
-import { CaretDown, Check, MapPin } from 'phosphor-react';
+import { MapPin } from 'phosphor-react';
 
 export function SelectInput() {
   return (
     <label htmlFor="sel" className="bg-gray-300 flex-1 flex items-center justify-between p-3 gap-1 relative rounded after:pointer-events-none">
       <MapPin size={24} className="text-red-500" />
-      <select id="sel" name="sel" className="outline-none bg-gray-300 min-w-[200px] w-full">
+      <select id="sel" name="sel" className="outline-none bg-gray-300 min-w-[150px] w-full">
         <option value="1">Text</option>
       </select>
     </label>
