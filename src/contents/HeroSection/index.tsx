ef52cbe0435b204903ccffb585b449544c69a228
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-02-28 21:48:40 -0300

    🎨 update HeroSection

--- src/contents/HeroSection/index.tsx
+++ src/contents/HeroSection/index.tsx
@@ -4,26 +4,26 @@ import { SelectInput } from "../../components/SelectInput";
 
 export function HeroSection() {
   return (
-    <section id="hero" className="min-h-[532px] bg-gray-300 flex flex-col items-center justify-center gap-10 relative z-10">
-      <header className="flex flex-col items-center gap-4 max-w-[645px] text-center">
+    <section id="hero" className="min-h-[532px] w-full bg-gray-300 flex flex-col items-center justify-center gap-10 relative z-10">
+      <header className="flex flex-col items-center gap-4 max-w-[645px] text-center px-2">
         <span className="font-medium uppercase text-red-500 tracking-widest">Find your block</span>
-        <h1 className="font-bold text-5xl text-gray-900">
+        <h1 className="font-bold text-2xl md:text-5xl text-gray-900">
           Encontre os <span className="text-violet-500">melhores blocos</span> de carnaval de 2023
         </h1>
       </header>
 
-      <div className="absolute inset-0 w-full flex justify-between item -z-10">
-        <img className="object-cover h-[330px]" src="/assets/ilustra-01.svg" />
-        <img className="self-end object-cover h-[330px]" src="/assets/ilustra-02.svg" />
+      <div className="absolute inset-0 flex justify-between item -z-10">
+        <img className="object-cover max-h-[330px]" src="/assets/ilustra-01.svg" />
+        <img className="self-end object-cover max-h-[330px]" src="/assets/ilustra-02.svg" />
       </div>
 
-      <div className="flex items-center justify-between gap-6 bg-white w-4/6 p-10 rounded-lg border border-gray-200">
+      <div className="flex items-center justify-between gap-6 flex-wrap md:flex-nowrap bg-white w-11/12 lg:w-4/6 p-6 md:p-10 rounded-lg border border-gray-200">
         <div className="min-w-[200px] bg-gray-300 flex-1 flex gap-1 p-3 rounded">
           <MagnifyingGlass size={24} className="text-red-500" />
           <input className="w-full bg-gray-300 outline-none text-sm" type="search" name="" id="" />
         </div>
         <SelectInput />
-        <Button>
+        <Button className="w-full lg:w-auto">
           Buscar agora
         </Button>
       </div>
